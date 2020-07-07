2b7030149a0ffea57b6119d7c0b65d577291c6f2
Philipp Schuster <phip1611@gmail.com>
2020-07-07 14:55:31 +0200

    Restored gpio_cdev example that was removed by mistake.

--- examples/src/bin/gpio_cdev.rs
+++ examples/src/bin/gpio_cdev.rs
@@ -1,22 +1,23 @@
-use std::thread::sleep;
-use std::time::Duration;
+use tm1637_example::{run_demo, sleep_busy_waiting};
 use tm1637_gpio_driver::gpio_api::setup_gpio_cdev;
-use tm1637_gpio_driver::TM1637Adapter;
 
-// example that defiantly works on Raspberry Pi
+/// Simple example that shows you how you can use the driver along with crate "gpio_cdev" to display
+/// content on the 4-digit 7-segment display by AZDelivery. This is the display shown in the gif of the readme.
+///
+/// This demo shows multiple kinds of using the display to show data.
 fn main() {
     // use any GPIO pin you want. This is the number of the pin on the board.
-    let (clk_pin, dio_pin) = (18, 23);
+    // The numbers in the example here are available on the Raspberry Pi for example.
+    let clk_pin = 18;
+    let dio_pin = 23;
 
-    let bit_delay_fn = Box::from(|| sleep(Duration::from_micros(10)));
+    // setup
+    // 100µs should be totally save; less could work; depends on cable length and other factors
+    // high frequencies are tricky; it even worked with zero waiting for me.. but better be safe!
+    let bit_delay_fn = || sleep_busy_waiting(10);
+    let bit_delay_fn = Box::from(bit_delay_fn);
+
     let tm1637display = setup_gpio_cdev(clk_pin, dio_pin, bit_delay_fn, "/dev/gpiochip0");
 
-    // display "1 2 3 4"
-    let data: [u8; 4] = [
-        TM1637Adapter::encode_digit(1),
-        TM1637Adapter::encode_digit(2),
-        TM1637Adapter::encode_digit(3),
-        TM1637Adapter::encode_digit(4),
-    ];
-    tm1637display.write_segments_raw(&data, 0);
+    run_demo(tm1637display);
 }
